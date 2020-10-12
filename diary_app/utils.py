1a6b31a7a8131c9eb94727a7cad8eafd3a26cb47
gylim0604 <gylim0604@hotmail.com>
2020-10-12 16:20:39 -0500

    added navigation button for calendar

--- diary_app/utils.py
+++ diary_app/utils.py
@@ -2,33 +2,37 @@ from datetime import datetime, timedelta
 from calendar import HTMLCalendar
 from .models import Entry
 
+# This calendar was made based on https://www.huiwenteo.com/normal/2018/07/24/django-calendar.html
 class Calendar(HTMLCalendar):
     def __init__(self, year=None, month=None):
         self.year = year
         self.month = month
         super(Calendar, self).__init__()
 
+    # formats entries of a day into a table 
     def formatday(self, day, entries):
         entries_per_day = entries.filter(entry_date__day = day)
-        d= ''
+        d = ''
         for entry in entries_per_day:
-            d += f'<li>{entry.title} </li>'
+            d += f'<li>{entry.title}</li>'
         
         if day != 0:
             return f"<td><span class='date'>{day}</span><ul> {d} </ul></td>"
         return '<td></td>'
     
+    # format the week in a table row
     def formatweek(self, theweek, entries):
         week = ''
         for d, weekday in theweek:
             week += self.formatday(d, entries)
-        return f'<tr>{week} </tr>'
+        return f'<tr> {week} </tr>'
     
     def formatmonth(self, withyear=True):
-        entries = Entry.objects.filter(entry_date__year=self.year)
+        entries = Entry.objects.filter(entry_date__year=self.year, entry_date__month=self.month)
         cal = f'<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
         cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
         cal += f'{self.formatweekheader()}\n'
+
         for week in self.monthdays2calendar(self.year,self.month):
             cal += f'{self.formatweek(week, entries)}\n'
         return cal
--- diary_app/views.py
+++ diary_app/views.py
@@ -1,4 +1,5 @@
 import datetime
+import calendar
 from django.shortcuts import render
 from django.views import generic
 from django.forms.models import modelform_factory
@@ -15,8 +16,10 @@ from diary_app.models import *
 from diary_app.utils import Calendar
 
 def index(request):
+    # handle POST request
     if request.method == 'POST':
         form = EntryForm(request.POST)
+        
         if form.is_valid():
             print(form.cleaned_data.get('content'))
             title = form.cleaned_data['title']
@@ -63,16 +66,33 @@ class CalendarView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        d = get_date(self.request.GET.get('day',None))
 
+        d = get_date(self.request.GET.get('month', None))
         cal = Calendar(d.year, d.month)
-
         html_cal = cal.formatmonth(withyear=True)
         context['calendar'] = mark_safe(html_cal)
+
+
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
         return context
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - datetime.timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' +str(prev_month.month)
+    return month
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+
+    next_month = last + datetime.timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' +str(next_month.month)
+    return month
 
 def get_date(req_day):
     if req_day:
         year, month = (int(x) for x in req_day.split('-'))
-        return date(year, month, day=1)
+        return datetime.date(year, month, day=1)
     return datetime.date.today()
