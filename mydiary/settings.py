ed5f0486cf8f04b55d76a5d86a94d0e87ac16d9a
gylim0604 <gylim0604@hotmail.com>
2020-11-08 23:28:30 -0600

    uhh

--- mydiary/settings.py
+++ mydiary/settings.py
@@ -112,16 +112,19 @@ LOGIN_URL ='/accounts/login'
 
 # TinyMCE config
 #Whitenoise for static files
-STATIC_URL = '/static/'
+WHITENOISE_USE_FINDERS = True
 
-# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
